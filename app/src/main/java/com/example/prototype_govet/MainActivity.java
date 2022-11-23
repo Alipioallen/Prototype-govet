502c5aee65db2b52116a1d5e90563f411e61a95d
Allen <alipioallen5@gmail.com>
2022-11-24 02:34:58 +0700

    BackEnd

--- app/src/main/java/com/example/prototype_govet/MainActivity.java
+++ app/src/main/java/com/example/prototype_govet/MainActivity.java
@@ -38,24 +38,72 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button payments;
-    EditText amounts;
-
     public static final String clientKey = "AfxOCHDgJp3kx4RSeJ1Rcr_3Fn3qnhebda3ZXICjOMXgwg_J9W1QktmaqtI00aUYBgJJMiWqo_WI-6ay";
-    public static final int PAYPAL_REQUEST_CODE = 123;
+    public static final int PAYPAL_REQUEST_CODE = 7171;
 
    // Paypal Configuration Object
     private static PayPalConfiguration config = new PayPalConfiguration()
            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
            .clientId(clientKey);
 
+    Button payments;
+    EditText amounts;
+
+    String amount;
+
+    @Override
+    protected void onDestroy() {
+        stopService(new Intent(this, PayPalService.class));
+        super.onDestroy();
+    }
+
+    //            // Start with mock environment.  When ready,
+//            // switch to sandbox (ENVIRONMENT_SANDBOX)
+//            // or live (ENVIRONMENT_PRODUCTION)
+//            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
+//            // on below line we are passing a client id.
+//            .clientId(clientKey);
+//    private EditText amountEdt;
+//    private TextView paymentTV;
+
+    private static final String TAG = "paymentExample";
+    /**
+     * - Set to PayPalConfiguration.ENVIRONMENT_PRODUCTION to move real money.
+     *
+     * - Set to PayPalConfiguration.ENVIRONMENT_SANDBOX to use your test credentials
+     * from https://developer.paypal.com
+     *
+     * - Set to PayPalConfiguration.ENVIRONMENT_NO_NETWORK to kick the tires
+     * without communicating to PayPal's servers.
+     */
+    private static final String CONFIG_ENVIRONMENT = PayPalConfiguration.ENVIRONMENT_NO_NETWORK;
+
+    // note that these credentials will differ between live & sandbox environments.
+    private static final String CONFIG_CLIENT_ID = "credentials from developer.paypal.com";
+
+    private static final int REQUEST_CODE_PAYMENT = 1;
+    private static final int REQUEST_CODE_FUTURE_PAYMENT = 2;
+    private static final int REQUEST_CODE_PROFILE_SHARING = 3;
+
+//    private static PayPalConfiguration config = new PayPalConfiguration()
+//            .environment(CONFIG_ENVIRONMENT)
+//            .clientId(CONFIG_CLIENT_ID)
+//            // The following are only used in PayPalFuturePaymentActivity.
+//            .merchantName("Example Merchant")
+//            .merchantPrivacyPolicyUri(Uri.parse("https://www.example.com/privacy"))
+//            .merchantUserAgreementUri(Uri.parse("https://www.example.com/legal"));
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        payments = findViewById(R.id.payments);
-        amounts = findViewById(R.id.amounts);
+        Intent intent = new Intent(this, PayPalService.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+        startService(intent);
+
+        payments = (Button) findViewById(R.id.payments);
+        amounts = (EditText) findViewById(R.id.amounts);
 
         payments.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,9 +117,10 @@ public class MainActivity extends AppCompatActivity {
 
     private void getPayment() {
 
-        String amount = amounts.getText().toString();
+        amount = amounts.getText().toString();
 
-        PayPalPayment payment = new PayPalPayment(new BigDecimal(String.valueOf(amount)), "PHP", "Learn", PayPalPayment.PAYMENT_INTENT_SALE);
+        PayPalPayment payment = new PayPalPayment(new BigDecimal(String.valueOf(amount)), "USD",
+                "Learn", PayPalPayment.PAYMENT_INTENT_SALE);
 
         Intent intent = new Intent(this,PaymentActivity.class);
         intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
@@ -83,32 +132,63 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+
         super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PAYPAL_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                PaymentConfirmation confirmation = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
 
-        if (requestCode==PAYPAL_REQUEST_CODE){
-            PaymentConfirmation configuration = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+                if (confirmation != null) {
+                    try {
+                        String paymentDetails = confirmation.toJSONObject().toString(4);
 
-            if (configuration!=null){
-                try {
-                    String paymentDetails = configuration.toJSONObject().toString(4);
+                        startActivity(new Intent(this, PaymentDetails.class)
+                                .putExtra("PaymentDetails", paymentDetails)
+                                .putExtra("PaymentAmount", amount)
+                        );
 
-                    JSONObject payObj = new JSONObject(paymentDetails);
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                        JSONObject payObj = new JSONObject(paymentDetails);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
 
-                    Log.e("Error", "Something went wrong");
+                        Log.e("Error", "Something went wrong");
+                    }
                 }
-            }
-        }
-        else if (requestCode==Activity.RESULT_CANCELED){
-            Log.i("Error", "Something went wrong");
-        }
-        else if (requestCode==PaymentActivity.RESULT_EXTRAS_INVALID){
-            Log.i("Payment", "Invalid Payment");
-        }
+            } else if (resultCode == Activity.RESULT_CANCELED)
+                Toast.makeText(this, "Cancel", Toast.LENGTH_SHORT).show();
+        } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID)
+            Toast.makeText(this, "Invalid", Toast.LENGTH_SHORT).show();
     }
 
+    public void onBuyPressed(View pressed) {
+        /*
+         * PAYMENT_INTENT_SALE will cause the payment to complete immediately.
+         * Change PAYMENT_INTENT_SALE to
+         *   - PAYMENT_INTENT_AUTHORIZE to only authorize payment and capture funds later.
+         *   - PAYMENT_INTENT_ORDER to create a payment for authorization and capture
+         *     later via calls from your server.
+         *
+         * Also, to include additional payment details and an item list, see getStuffToBuy() below.
+         */
+        PayPalPayment thingToBuy = getThingToBuy(PayPalPayment.PAYMENT_INTENT_SALE);
 
+        /*
+         * See getStuffToBuy(..) for examples of some available payment options.
+         */
 
+        Intent intent = new Intent(MainActivity.this, PaymentActivity.class);
 
+        // send the same configuration for restart resiliency
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, thingToBuy);
+
+        startActivityForResult(intent, REQUEST_CODE_PAYMENT);
+    }
+
+    private PayPalPayment getThingToBuy(String paymentIntent) {
+        return new PayPalPayment(new BigDecimal("0.01"), "USD", "sample item",
+                paymentIntent);
+    }
 }
