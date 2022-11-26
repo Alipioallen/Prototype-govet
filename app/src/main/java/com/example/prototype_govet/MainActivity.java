1b166164bd800c24fca680c1e92c78eedbc1485b
Alipioallen <111748932+Alipioallen@users.noreply.github.com>
2022-11-26 22:29:08 +0800

    start button

--- app/src/main/java/com/example/prototype_govet/MainActivity.java
+++ app/src/main/java/com/example/prototype_govet/MainActivity.java
@@ -2,193 +2,31 @@ package com.example.prototype_govet;
 
 
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.paypal.android.sdk.payments.PayPalAuthorization;
-import com.paypal.android.sdk.payments.PayPalConfiguration;
-import com.paypal.android.sdk.payments.PayPalFuturePaymentActivity;
-import com.paypal.android.sdk.payments.PayPalItem;
-import com.paypal.android.sdk.payments.PayPalOAuthScopes;
-import com.paypal.android.sdk.payments.PayPalPayment;
-import com.paypal.android.sdk.payments.PayPalPaymentDetails;
-import com.paypal.android.sdk.payments.PayPalProfileSharingActivity;
-import com.paypal.android.sdk.payments.PayPalService;
-import com.paypal.android.sdk.payments.PaymentActivity;
-import com.paypal.android.sdk.payments.PaymentConfirmation;
-import com.paypal.android.sdk.payments.ShippingAddress;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String clientKey = "AfxOCHDgJp3kx4RSeJ1Rcr_3Fn3qnhebda3ZXICjOMXgwg_J9W1QktmaqtI00aUYBgJJMiWqo_WI-6ay";
-    public static final int PAYPAL_REQUEST_CODE = 7171;
-
-    // Paypal Configuration Object
-    private static PayPalConfiguration config = new PayPalConfiguration()
-            .environment(PayPalConfiguration.ENVIRONMENT_NO_NETWORK)
-            .clientId(clientKey);
-
-    Button payments;
-    EditText amounts;
-    String amount;
-
-    @Override
-    protected void onDestroy() {
-        stopService(new Intent(this, PayPalService.class));
-        super.onDestroy();
-    }
-
-    //            // Start with mock environment.  When ready,
-//            // switch to sandbox (ENVIRONMENT_SANDBOX)
-//            // or live (ENVIRONMENT_PRODUCTION)
-//            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
-//            // on below line we are passing a client id.
-//            .clientId(clientKey);
-//    private EditText amountEdt;
-//    private TextView paymentTV;
-
-    private static final String TAG = "paymentExample";
-    /**
-     * - Set to PayPalConfiguration.ENVIRONMENT_PRODUCTION to move real money.
-     *
-     * - Set to PayPalConfiguration.ENVIRONMENT_SANDBOX to use your test credentials
-     * from https://developer.paypal.com
-     *
-     * - Set to PayPalConfiguration.ENVIRONMENT_NO_NETWORK to kick the tires
-     * without communicating to PayPal's servers.
-     */
-    private static final String CONFIG_ENVIRONMENT = PayPalConfiguration.ENVIRONMENT_NO_NETWORK;
-
-    // note that these credentials will differ between live & sandbox environments.
-    private static final String CONFIG_CLIENT_ID = "credentials from developer.paypal.com";
-
-    private static final int REQUEST_CODE_PAYMENT = 1;
-    private static final int REQUEST_CODE_FUTURE_PAYMENT = 2;
-    private static final int REQUEST_CODE_PROFILE_SHARING = 3;
-
-//    private static PayPalConfiguration config = new PayPalConfiguration()
-//            .environment(CONFIG_ENVIRONMENT)
-//            .clientId(CONFIG_CLIENT_ID)
-//            // The following are only used in PayPalFuturePaymentActivity.
-//            .merchantName("Example Merchant")
-//            .merchantPrivacyPolicyUri(Uri.parse("https://www.example.com/privacy"))
-//            .merchantUserAgreementUri(Uri.parse("https://www.example.com/legal"));
+    Button starts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, PayPalService.class);
-        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
-        startService(intent);
+        starts = findViewById(R.id.Start);
 
-        payments = (Button) findViewById(R.id.payments);
-        amounts = (EditText) findViewById(R.id.amounts);
-
-        payments.setOnClickListener(new View.OnClickListener() {
+        starts.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
-                getPayment();
-
+            public void onClick(View v) {
+                startActivity(new Intent(getApplication(), Pyment.class));
+                finish();
             }
         });
-    }
-
-    private void getPayment() {
-
-        amount = amounts.getText().toString();
-
-        PayPalPayment payment = new PayPalPayment(new BigDecimal(String.valueOf(amount)), "USD",
-                "Appointment Fee", PayPalPayment.PAYMENT_INTENT_SALE);
-
-        Intent intent = new Intent(this,PaymentActivity.class);
-        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
-        intent.putExtra(PaymentActivity.EXTRA_PAYMENT,payment);
-
-        startActivityForResult(intent,PAYPAL_REQUEST_CODE);
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-
-
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PAYPAL_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                PaymentConfirmation confirmation = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
-
-                if (confirmation != null) {
-                    try {
-                        String paymentDetails = confirmation.toJSONObject().toString(4);
-
-                        startActivity(new Intent(this, PaymentDetails.class)
-                                .putExtra("PaymentDetails", paymentDetails)
-                                .putExtra("PaymentAmount", amount)
-                        );
-
-                        JSONObject payObj = new JSONObject(paymentDetails);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-
-                        Log.e("Error", "Something went wrong");
-                    }
-                }
-            } else if (resultCode == Activity.RESULT_CANCELED)
-                Toast.makeText(this, "Cancel", Toast.LENGTH_SHORT).show();
-        } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID)
-            Toast.makeText(this, "Invalid", Toast.LENGTH_SHORT).show();
-    }
-
-    public void onBuyPressed(View pressed) {
-        /*
-         * PAYMENT_INTENT_SALE will cause the payment to complete immediately.
-         * Change PAYMENT_INTENT_SALE to
-         *   - PAYMENT_INTENT_AUTHORIZE to only authorize payment and capture funds later.
-         *   - PAYMENT_INTENT_ORDER to create a payment for authorization and capture
-         *     later via calls from your server.
-         *
-         * Also, to include additional payment details and an item list, see getStuffToBuy() below.
-         */
-        PayPalPayment thingToBuy = getThingToBuy(PayPalPayment.PAYMENT_INTENT_SALE);
-
-        /*
-         * See getStuffToBuy(..) for examples of some available payment options.
-         */
-
-        Intent intent = new Intent(MainActivity.this, PaymentActivity.class);
-
-        // send the same configuration for restart resiliency
-        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
-
-        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, thingToBuy);
-
-        startActivityForResult(intent, REQUEST_CODE_PAYMENT);
-    }
-
-    private PayPalPayment getThingToBuy(String paymentIntent) {
-        return new PayPalPayment(new BigDecimal("0.01"), "USD", "sample item",
-                paymentIntent);
     }
 }
