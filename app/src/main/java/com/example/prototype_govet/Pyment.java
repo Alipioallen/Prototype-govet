2d1eaaac8a6e36783cbca88ff7c0b60e122c7ce1
Alipioallen <111748932+Alipioallen@users.noreply.github.com>
2022-11-26 22:16:30 +0800

    payment

---
+++ app/src/main/java/com/example/prototype_govet/Pyment.java
@@ -0,0 +1,14 @@
+package com.example.prototype_govet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pyment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pyment);
+    }
+}
